ecd0d22a00b4ff2305f9b3343f0d195e85eeb0c8
RahulNavneeth <50259284+RahulNavneeth@users.noreply.github.com>
2022-11-15 18:34:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,32 +3,23 @@ keepAlive();
 require("dotenv");
 const { IgApiClient } = require("instagram-private-api");
 const USERNAME = process.env.INSTA_USERNAME;
+const PASSWORD = process.env.PASSWORD;
 const ig = new IgApiClient();
 const cron = require("node-cron");
 ig.state.generateDevice(USERNAME);
 
-const formateTime = (date) => {
-  var currentTime = date;
-  var currentOffset = currentTime.getTimezoneOffset();
-
-  var ISTOffset = 330;
-  var ISTTime = new Date(
-    currentTime.getTime() + (ISTOffset + currentOffset) * 60000
-  ).toLocaleString();
-
-  return ISTTime;
-};
-
 (async () => {
   // await ig.simulate.preLoginFlow();
-  await ig.account.login(USERNAME, USERNAME.split("m").join("") + "14");
+  await ig.account.login(USERNAME, PASSWORD);
+  
+  // Scheduled every hour -> (for custom scheduling head to - [ https://crontab.guru/ ] )
   cron.schedule("0 * * * * *", async () => {
+    // (or) could have schduled every minute and use if condition to stop at certain time (new Date()... N Stuff)
+    
     while (formateTime(new Date()).split(" ")[1].split(":")[1] === "00") {
       process.nextTick(async () => await ig.simulate.postLoginFlow());
       const setBio = await ig.account.setBiography(
-        `Hey, Its ${formateTime(new Date()).split(" ")[1].split(":")[0]}${
-          formateTime(new Date()).split(" ")[2]
-        } and you are here.`
+        `----YOUR MESSAGE----`
       );
       console.log(setBio);
       console.log(formateTime(new Date()));
